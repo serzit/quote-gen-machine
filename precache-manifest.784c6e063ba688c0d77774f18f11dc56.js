69030cba3a01e530f92e4c2d732f6ba6271b4d7d
serzit <serzit@yandex.ru>
2020-08-03 20:12:17 +0300

    Updates

---
+++ precache-manifest.784c6e063ba688c0d77774f18f11dc56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfc6f2fb415be6ac09acbf5c89fa32a",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "ab5c6b96f10bb91c433a",
+    "url": "/quote-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a24dfb0fbd82a8d500f2",
+    "url": "/quote-generator/static/js/2.43d245e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.43d245e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5c6b96f10bb91c433a",
+    "url": "/quote-generator/static/js/main.ab837eb5.chunk.js"
+  },
+  {
+    "revision": "8a3eaa8a66a077c69940",
+    "url": "/quote-generator/static/js/runtime-main.c813f35b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quote-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
